168795fed708869ca9cacf35b22e53a8eec8f350
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-05-23 20:48:31 -0700

    Recoded finite automotas
    Now, instead of using individual objects for each node, there is just
    one object which maintains a table of transitions. This is closer to
    what will be in the final c++ implementation.

--- compiler_v2/finite_automota.py
+++ compiler_v2/finite_automota.py
@@ -1,81 +1,70 @@
-class DeterminateNode:
-    def __init__(self, transitions = {}):
-        self.transitions = transitions
+class DeterminateFiniteAutomota:
+    def __init__(self):
+        self.transitions = {}
+        self.node_counter = -1
     
-    def __getitem__(self, key):
-        if(key in self.transitions):
-            return self.transitions[key]
-        else:
-            return None
+    def add_node(self):
+        self.node_counter += 1
+        self.transitions[self.node_counter] = {}
+        return self.node_counter
     
-    def __setitem__(self, key, value):
-        assert type(key) is str
-        assert len(key) == 1
-        if value is None:
-            if(key in self.transitions):
-                del self.transitions[key]
-        else:
-            assert type(value) is DeterminateNode
-            self.transitions[key] = value
-    
-    def __delitem__(self, key):
-        assert type(key) is str
-        assert len(key) == 1
-        if(key in self.transitions):
-            del self.transitions[key]        
-        
-DNode = DeterminateNode
-
-class NondeterminateNode:
-    uuid_counter = 0
-    def __init__(self, transitions = {}):
-        self.transitions = transitions
-        self.uuid = NondeterminateNode.uuid_counter
-        NondeterminateNode.uuid_counter += 1
-    
-    def __getitem__(self, key):
-        if(key not in self.transitions):
-            self.transitions[key] = []
-        return self.transitions[key]
-    
-    def __setitem__(self, key, value):
-        assert type(key) is str
-        assert len(key) <= 1
-        if value is None:
-            if(key in self.transitions):
-                del self.transitions[key]
-        else:
-            if(isinstance(value, NondeterminateNode)):
-                self.transitions[key] = [value]
-            elif(type(value) is list and min([isinstance(i, NondeterminateNode) for i in value])):
-                self.transitions[key] = value
-            else:
-                assert False
-    
-    def __delitem__(self, key):
-        assert type(key) is str
-        assert len(key) <= 1
-        if(key in self.transitions):
-            del self.transitions[key]
-    
-    def __radd__(self, addend):
-        if(type(addend) is list):
-            return addend + [self]
-        elif(isinstance(addend, NondeterminateNode)):
-            return [self, addend]
+    def set_transition(self, start_state, trigger, end_state):
+        assert trigger != '' and trigger != '\x00' # empty string transitions not allowed in DFAs
+        # Add extra nodes if they are not already part of the graph.
+        while(self.node_counter < start_state):
+            self.add_node()
+        while(self.node_counter < end_state):
+            self.add_node()
+        self.transitions[start_state][trigger] = end_state
     
     def __str__(self):
-        return 'Node #' + str(self.uuid) + ' (' + str(sum([len(i) for i in self.transitions.items()])) + ' transitions)'
-    
-    def __repr__(self):
-        return str(self)
+        out = '===== BEGIN DFA DESCRIPTION =====\n'
+        out += str(self.node_counter + 1) + ' node(s)\n'
+        for i in range(self.node_counter + 1):
+            for trigger in self.transitions[i].keys():
+                out += str(i) + ' -> "' + trigger + '" -> ' + str(self.transitions[i][trigger]) + '\n'
+        out += '====== END DFA DESCRIPTION ======'
+        return out
 
-NNode = NondeterminateNode
+class NondeterminateFiniteAutomota:
+    def __init__(self):
+        self.transitions = {}
+        self.node_counter = -1
+    
+    def add_node(self):
+        self.node_counter += 1
+        self.transitions[self.node_counter] = {}
+        return self.node_counter
+    
+    def add_transition(self, start_state, trigger, end_state):
+        # Add extra nodes if they are not already part of the graph.
+        while(self.node_counter < start_state):
+            self.add_node()
+        while(self.node_counter < end_state):
+            self.add_node()
+        if(trigger not in self.transitions[start_state].keys()):
+            self.transitions[start_state][trigger] = []
+        self.transitions[start_state][trigger].append(end_state)
+    
+    def __str__(self):
+        out = '===== BEGIN NFA DESCRIPTION =====\n'
+        out += str(self.node_counter + 1) + ' node(s)\n'
+        for i in range(self.node_counter + 1):
+            for trigger in self.transitions[i].keys():
+                out += str(i) + ' -> "' + trigger + '" -> ' + str(self.transitions[i][trigger]) + '\n'
+        out += '====== END NFA DESCRIPTION ======'
+        return out
+    
+    def convert_to_dfa(self):
+        pass
 
 if __name__ == '__main__':
-    a, b, c = NNode(), NNode(), NNode()
-    a['1'].append(b)
-    a['1'] += c
+    nfa = NondeterminateFiniteAutomota()
+    a, b, c = [nfa.add_node() for i in range(3)]
+    nfa.add_transition(a, '1', b)
+    nfa.add_transition(a, '1', c)
+    nfa.add_transition(b, '2', c)
+    nfa.add_transition(c, '3', a)
     
-    print(a['1'])
+    print(nfa)
 
