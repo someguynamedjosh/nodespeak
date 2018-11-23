20044989dfbd3b40396ad3750e778d43752ebcaf
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-11-22 21:18:15 -0800

    Fixed parser error detection.

--- source/x3test/parser.cpp
+++ source/x3test/parser.cpp
@@ -14,8 +14,10 @@ BOOST_SPIRIT_INSTANTIATE(root_rule_type, iterator_type, context_type)
 root_type parse(std::string input) {
     ast::Expression result;
     
-    bool success = phrase_parse(input.begin(), input.end(), root_rule,
+    iterator_type start = input.begin(), end = input.end();
+    bool success = phrase_parse(start, end, root_rule,
         x3::ascii::space, result);
+    success &= start == end;
     std::cout << success << std::endl;
     return result;
 }
