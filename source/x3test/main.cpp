82a929772127eda59f388422b224b1a111fd387f
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-11-23 11:28:50 -0800

    Added printer to main.cpp

--- source/x3test/main.cpp
+++ source/x3test/main.cpp
@@ -3,10 +3,12 @@
 
 #include "ast.hpp"
 #include "ast_adapted.hpp"
+#include "ast_printer.hpp"
 #include "parser.hpp"
 
 int main() {
-    std::string code = "sin(one + two)";
+    std::string code = "Int a, b, c;";
     auto result = waveguide::parser::parse(code);
+    waveguide::ast::print_ast(result);
     return 0;
 }
