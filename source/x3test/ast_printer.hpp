d30513315ef9c18626b44e6a0e393047e3335977
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-11-23 09:45:16 -0800

    Added new grammar components to AstPrinter.

--- source/x3test/ast_printer.hpp
+++ source/x3test/ast_printer.hpp
@@ -14,7 +14,7 @@ struct AstPrinter: boost::static_visitor<> {
 
     AstPrinter(int indent): indent(indent) { }
 
-    void write_indent() const {
+    void print_indent() const {
         for (int i = 0; i < indent; i++) {
             std::cout << " ";
         }
@@ -66,13 +66,61 @@ struct AstPrinter: boost::static_visitor<> {
         std::cout << expr.name;
     }
 
+    void operator()(PlainDataType const&type) const {
+        std::cout << "type:" << type.name << std::endl;
+    }
+
+    void operator()(ArrayDataType const&type) const {
+        recurse(type.base);
+        std::cout << '[';
+        recurse(type.size);
+        std::cout << ']';
+    }
+
+    void operator()(FunctionStatement const&stat) const {
+        print_indent();
+        (*this)(stat.func_call);
+        std::cout << ';' << std::endl;
+    }
+
+    void operator()(AssignStatement const&stat) const {
+        print_indent();
+        (*this)(stat.assign_to);
+        std::cout << " = ";
+        recurse(stat.value);
+        std::cout << ';' << std::endl;
+    }
+
+    void operator()(PlainVarDec const&dec) const {
+        std::cout << dec.name;
+    }
+
+    void operator()(InitVarDec const&dec) const {
+        std::cout << dec.name << " = ";
+        recurse(dec.value);
+    }
+
+    void operator()(VarDecStatement const&stat) const {
+        print_indent();
+        std::cout << "declare, ";
+        recurse(stat.type);
+        bool first = true;
+        for (auto const&dec : stat.var_decs) {
+            if (!first) std::cout << ", ";
+            first = false;
+            recurse(dec);
+        }
+        std::cout << ';' << std::endl;
+    }
+
     template<typename Visitable>
     void recurse(Visitable &to_print) const {
         boost::apply_visitor(AstPrinter{indent + INDENT_WIDTH}, to_print);
     }
 };
 
-inline void print_ast(Expression const& expr) {
+template<typename Visitable>
+inline void print_ast(Visitable const& expr) {
     boost::apply_visitor(AstPrinter{0}, expr);
 }
 
