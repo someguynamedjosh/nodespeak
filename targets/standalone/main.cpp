94cab71c65cf510bfb04174dca27a45fcad71d51
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-11-27 06:48:20 -0800

    Made the standalone compiler accept command-line input, for easy testing

--- targets/standalone/main.cpp
+++ targets/standalone/main.cpp
@@ -4,8 +4,14 @@
 #include "waveguide/parser.hpp"
 #include "waveguide/ast_util.hpp"
 
-int main() {
-    std::string code = "Int a, b, c;";
+int main(int argc, char **argv) {
+    std::string code = "";
+    // First argument is program name / path. 
+    for (int i = 1; i < argc; i++) {
+        code += std::string{argv[i]} + " ";
+    }
+    std::cout << "Compiling code:" << std::endl;
+    std::cout << code << std::endl;
     auto result = waveguide::parser::parse(code);
     std::cout << 
         (result.error ? "Compile failed!" : "Compile suceeded!") << std::endl;
