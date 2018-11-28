0879b59b8c66ddeef7c2fe21dac7ffdbf470cfcc
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-11-28 14:08:12 -0800

    Made standalone parser read from cin and files.

--- targets/standalone/main.cpp
+++ targets/standalone/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -5,10 +6,26 @@
 #include "waveguide/ast_util.hpp"
 
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Usage:" << std::endl;
+        std::cout << "waveguide_standalone [source]" << std::endl;
+        std::cout << "source is either a file name to retrieve code from, or a";
+        std::cout << "dash to read from cin." << std::endl;
+        return 1;
+    }
     std::string code = "";
-    // First argument is program name / path. 
-    for (int i = 1; i < argc; i++) {
-        code += std::string{argv[i]} + " ";
+    if (argv[1][0] == '-' && argv[1][1] == '\x00') {
+        std::cout << "Reading code from stdin." << std::endl;
+        std::string line;
+        while (std::getline(std::cin, line)) {
+            code += line + "\n";
+        }
+    } else {
+        std::cout << "Reading code from file " << argv[1] << "." << std::endl;
+        std::ifstream file{argv[1]};
+        std::stringstream temp_buf;
+        temp_buf << file.rdbuf();
+        code = temp_buf.str();
     }
     std::cout << "Compiling code:" << std::endl;
     std::cout << code << std::endl;
