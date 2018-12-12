bcfb0519954d623d92841c16f04c6def7f980ece
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-12-11 20:41:11 -0800

    Fixed syntax and formatting errors in out_parse.py.

--- out_parse.py
+++ out_parse.py
@@ -3,12 +3,13 @@ import sys
 tokens = {}
 for line in sys.stdin:
     if (' is ' in line and ' is Scope' not in line):
-        name = line[line.find(' is ')+3:]
+        sline = line.strip()
+        name = sline[sline.find(' is ')+3:]
         if (name[-1] == ':'):
-            name = name[;-1]
-        hexv = line[:line.find(' is ')].strip()
+            name = name[:-1]
+        hexv = sline[:sline.find(' is ')].strip()
         tokens[hexv] = name
-    else:
-        for key, value in tokens.items():
-            line = line.replace(key, value)
-        print(line)
+    line = line.rstrip()
+    for key, value in tokens.items():
+        line = line.replace(key, value)
+    print(line)
