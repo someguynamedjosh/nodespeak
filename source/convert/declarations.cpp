985067357addc868a9bcc7f6e1867601c6ba2396
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-12-20 14:43:01 -0800

    Added conversion logic for array types.

--- source/convert/declarations.cpp
+++ source/convert/declarations.cpp
@@ -14,6 +14,15 @@ void AstConverter::operator()(function_dec const&dec) const {
 void AstConverter::operator()(data_type const&type) const {
     // TODO: add logic for array types.
     data->current_type = lookup_type(type.name);
+    for (auto size : type.array_sizes) {
+        recurse(size);
+        // TODO: Error if value is not known.
+        data->current_type = SP<intr::array_data_type>{
+            new intr::array_data_type{
+                data->current_type, *data->current_value->data_as_int()
+            }
+        };
+    }
 }
 
 }
