606476145dec60bd087a32dbef33fda79b06eb9a
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-12-22 13:43:36 -0800

    Fixed some formatting bugs.

--- out_parse.py
+++ out_parse.py
@@ -2,14 +2,15 @@ import sys
 
 tokens = {}
 for line in sys.stdin:
-    if (' is ' in line and ' is Scope' not in line):
+    line = line.rstrip()
+    if (' is ' in line and ' is Scope' not in line and 'Lambda' not in line):
         sline = line.strip()
-        name = sline[sline.find(' is ')+3:]
+        name = sline[sline.find(' is ')+3:].strip()
         if (name[-1] == ':'):
             name = name[:-1]
-        hexv = sline[:sline.find(' is ')].strip()
+        hexv = sline[:sline.find(' is ')].split()[-1].strip()
         tokens[hexv] = name
-    line = line.rstrip()
-    for key, value in tokens.items():
-        line = line.replace(key, value)
+    else:
+        for key, value in tokens.items():
+            line = line.replace(key, value)
     print(line)
