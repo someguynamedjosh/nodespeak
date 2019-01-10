9ec506db455cb9c7058aea0e094097902362646c
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2019-01-10 08:02:04 -0800

    Added implementations for helper functions in vague data types.

--- source/intermediate/type_template.cpp
+++ source/intermediate/type_template.cpp
@@ -19,6 +19,15 @@ void vague_number_expression::print_repr(std::ostream &stream) const {
 void vague_number_expression::collect_new_vars(std::vector<std::string> &list) 
     const { }
 
+bool vague_number_expression::is_constant() const {
+    return true;
+}
+
+int vague_number_expression::do_algebra(possible_value_table &values,
+    int final_value) const {
+    return value;
+}
+
 int vague_number_expression::get_value() const {
     return value;
 }
@@ -35,6 +44,16 @@ void vague_value_expression::collect_new_vars(std::vector<std::string> &list)
     list.push_back(name);
 }
 
+bool vague_value_expression::is_constant() const {
+    return true;
+}
+
+int vague_value_expression::do_algebra(possible_value_table &values,
+    int final_value) const {
+    values[name].push_back(final_value);
+    return final_value;
+}
+
 std::string vague_value_expression::get_name() const {
     return name;
 }
@@ -49,6 +68,17 @@ void vague_known_value_expression::print_repr(std::ostream &stream) const {
 void vague_known_value_expression::collect_new_vars(
     std::vector<std::string> &list) const { }
 
+bool vague_known_value_expression::is_constant() const {
+    assert(real_value->is_value_known());
+    return true;
+}
+
+int vague_known_value_expression::do_algebra(possible_value_table &values,
+    int final_value) const {
+    assert(real_value->is_value_known());
+    return *real_value->data_as_int();
+}
+
 value_ptr vague_known_value_expression::get_real_value() const {
     return real_value;
 }
@@ -60,6 +90,11 @@ vague_operation_expression::vague_operation_expression(vague_expression_ptr a,
     vague_expression_ptr b)
     : operands{a, b} { }
 
+std::vector<vague_expression_ptr> 
+    const&vague_operation_expression::get_operands() const {
+    return operands;
+}
+
 void vague_operation_expression::collect_new_vars(
     std::vector<std::string> &list) const {
     for (auto operand : operands) {
@@ -67,9 +102,12 @@ void vague_operation_expression::collect_new_vars(
     }
 }
 
-std::vector<vague_expression_ptr> 
-    const&vague_operation_expression::get_operands() const {
-    return operands;
+bool vague_operation_expression::is_constant() const {
+    bool result = true;
+    for (auto operand : operands) {
+        result &= operand->is_constant();
+    }
+    return result;
 }
 
 vague_negation_expression::vague_negation_expression(vague_expression_ptr input)
@@ -80,6 +118,12 @@ void vague_negation_expression::print_repr(std::ostream &stream) const {
     stream << "(-";
     ops[0]->print_repr(stream);
     stream << ")";
+}
+
+int vague_negation_expression::do_algebra(possible_value_table &table,
+    int final_value) const {
+    get_operands()[0]->do_algebra(table, -final_value);
+    return final_value;
 }
 
 vague_add_expression::vague_add_expression(vague_expression_ptr a, 
@@ -95,6 +139,21 @@ void vague_add_expression::print_repr(std::ostream &stream) const {
     stream << ")";
 }
 
+int vague_add_expression::do_algebra(possible_value_table &table,
+    int final_value) const {
+    auto a = get_operands()[0], b = get_operands()[1];
+    if (a->is_constant()) {
+        final_value -= a->do_algebra(table, final_value);
+        b->do_algebra(table, final_value);
+    } else if (b->is_constant()) {
+        final_value -= b->do_algebra(table, final_value);
+        a->do_algebra(table, final_value);
+    } else {
+        return 0;
+    }
+    return final_value;
+}
+
 vague_subtract_expression::vague_subtract_expression(vague_expression_ptr a, 
     vague_expression_ptr b)
     : vague_operation_expression{a, b} { }
@@ -106,6 +165,21 @@ void vague_subtract_expression::print_repr(std::ostream &stream) const {
     stream << " - ";
     ops[1]->print_repr(stream);
     stream << ")";
+}
+
+int vague_subtract_expression::do_algebra(possible_value_table &table,
+    int final_value) const {
+    auto a = get_operands()[0], b = get_operands()[1];
+    if (a->is_constant()) {
+        final_value = a->do_algebra(table, final_value);
+        b->do_algebra(table, final_value);
+    } else if (b->is_constant()) {
+        final_value += b->do_algebra(table, final_value);
+        a->do_algebra(table, final_value);
+    } else {
+        return 0;
+    }
+    return final_value;
 }
 
 vague_multiply_expression::vague_multiply_expression(vague_expression_ptr a,
@@ -121,6 +195,27 @@ void vague_multiply_expression::print_repr(std::ostream &stream) const {
     stream << ")";
 }
 
+int vague_multiply_expression::do_algebra(possible_value_table &table,
+    int final_value) const {
+    auto a = get_operands()[0], b = get_operands()[1];
+    if (a->is_constant()) {
+        int divisor = a->do_algebra(table, final_value);
+        if (final_value % divisor != 0) {
+            return 0;
+        }
+        b->do_algebra(table, final_value / divisor);
+    } else if (b->is_constant()) {
+        int divisor = b->do_algebra(table, final_value);
+        if (final_value % divisor != 0) {
+            return 0;
+        }
+        a->do_algebra(table, final_value / divisor);
+    } else {
+        return 0;
+    }
+    return final_value;
+}
+
 vague_divide_expression::vague_divide_expression(vague_expression_ptr a, 
     vague_expression_ptr b)
     : vague_operation_expression{a, b} { }
@@ -132,6 +227,24 @@ void vague_divide_expression::print_repr(std::ostream &stream) const {
     stream << " / ";
     ops[1]->print_repr(stream);
     stream << ")";
+}
+
+int vague_divide_expression::do_algebra(possible_value_table &table,
+    int final_value) const {
+    auto a = get_operands()[0], b = get_operands()[1];
+    if (a->is_constant()) {
+        int top = a->do_algebra(table, final_value);
+        if (top % final_value != 0) {
+            return 0;
+        }
+        b->do_algebra(table, top / final_value);
+    } else if (b->is_constant()) {
+        int divisor = b->do_algebra(table, final_value);
+        a->do_algebra(table, final_value * divisor);
+    } else {
+        return 0;
+    }
+    return final_value;
 }
 
 
@@ -151,11 +264,17 @@ void vague_basic_data_type::collect_new_types(std::vector<std::string> &list)
     list.push_back(name);
 }
 
+bool vague_basic_data_type::fill_tables(possible_value_table &value_table,
+    data_type_table &type_table, const_data_type_ptr real_type) const {
+    type_table[name].push_back(real_type);
+    return true;
+}
+
 std::string vague_basic_data_type::get_name() const {
     return name;
 }
 
-vague_known_data_type::vague_known_data_type(data_type_ptr real_type)
+vague_known_data_type::vague_known_data_type(const_data_type_ptr real_type)
     : real_type{real_type} { }
 
 void vague_known_data_type::print_repr(std::ostream &stream) const {
@@ -168,11 +287,17 @@ void vague_known_data_type::collect_new_vars(std::vector<std::string> &list)
 void vague_known_data_type::collect_new_types(std::vector<std::string> &list)
     const { }
 
-data_type_ptr vague_known_data_type::get_real_type() const {
+bool vague_known_data_type::fill_tables(possible_value_table &value_table,
+    data_type_table &type_table, const_data_type_ptr real_type) const {
+    // TODO: Check for equality between real_type and this->real_type.
+    return true;
+}
+
+const_data_type_ptr vague_known_data_type::get_real_type() const {
     return real_type;
 }
 
-vague_array_data_type::vague_array_data_type(vague_data_type_ptr base, 
+vague_array_data_type::vague_array_data_type(vague_const_data_type_ptr base, 
     vague_expression_ptr size)
     : base(base), size(size) { }
 
@@ -193,7 +318,20 @@ void vague_array_data_type::collect_new_types(std::vector<std::string> &list)
     base->collect_new_types(list);
 }
 
-vague_data_type_ptr vague_array_data_type::get_base_type() const {
+bool vague_array_data_type::fill_tables(possible_value_table &value_table,
+    data_type_table &type_table, const_data_type_ptr real_type) const {
+    auto array_type = std::dynamic_pointer_cast<const array_data_type>(
+        real_type
+    );
+    if (!array_type) return false;
+    int asize = size->do_algebra(value_table, array_type->get_array_length());
+    if (asize != array_type->get_array_length()) return false;
+    base->fill_tables(value_table, type_table, std::static_pointer_cast
+        <const data_type>(array_type->get_element_type())
+    );
+}
+
+vague_const_data_type_ptr vague_array_data_type::get_base_type() const {
     return base;
 }
 
