551ad2b7ed13a4d792ab1cae3e90f0b7c0759cd0
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2019-10-21 11:30:46 -0700

    Added trivial instructions.

--- src/trivial/instruction.rs
+++ src/trivial/instruction.rs
@@ -1,3 +1,90 @@
-pub enum Instruction {
+use crate::trivial::{LabelId, VariableId};
 
+pub enum Condition {
+    LessThan,
+    GreaterThan,
+    LessThanOrEqual,
+    GreaterThanOrEqual,
+    Equal,
+    NotEqual,
+    Unconditional,
+}
+
+pub enum Instruction {
+    Move {
+        from: VariableId,
+        to: VariableId,
+    },
+
+    AddI {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    SubI {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    MulI {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    DivI {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+
+    AddF {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    SubF {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    MulF {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    DivF {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+
+    And {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    Or {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    Xor {
+        a: VariableId,
+        b: VariableId,
+        x: VariableId,
+    },
+    Not {
+        a: VariableId,
+        x: VariableId,
+    },
+
+    Compare {
+        a: VariableId,
+        b: VariableId,
+    },
+    Label(LabelId),
+    Jump {
+        label: LabelId,
+        condition: Condition,
+    },
 }
--- src/trivial/program.rs
+++ src/trivial/program.rs
@@ -1,5 +1,25 @@
 use crate::trivial::Instruction;
 
+use std::fmt::{self, Debug, Formatter};
+
+#[derive(Clone, Copy)]
+pub struct VariableId(usize);
+
+impl Debug for VariableId {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        write!(formatter, "t{}", self.0)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct LabelId(usize);
+
+impl Debug for LabelId {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        write!(formatter, "l{}", self.0)
+    }
+}
+
 pub struct Program {
-    instructions: Vec<Instruction>
+    instructions: Vec<Instruction>,
 }
