d239823797291c7094ce75b203f44a1fdd99a4c9
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2019-10-21 10:01:09 -0700

    Fleshed out trivial variables.

--- src/trivial/mod.rs
+++ src/trivial/mod.rs
@@ -1,9 +1,7 @@
-mod data_type;
 mod instruction;
 mod program;
 mod variable;
 
-pub use data_type::*;
 pub use instruction::*;
 pub use program::*;
 pub use variable::*;
--- src/trivial/variable.rs
+++ src/trivial/variable.rs
@@ -1,5 +1,67 @@
-use crate::trivial::DataType;
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum VariableType {
+    F32,
+    I32,
+}
 
+#[derive(Clone, Debug)]
 pub struct Variable {
-    data_type: DataType
+    variable_type: VariableType,
+    dimensions: Vec<usize>,
+}
+
+impl Variable {
+    pub fn new(typ: VariableType) -> Variable {
+        Variable {
+            variable_type: typ,
+            dimensions: vec![],
+        }
+    }
+
+    pub fn f32() -> Variable {
+        Self::new(VariableType::F32)
+    }
+
+    pub fn i32() -> Variable {
+        Self::new(VariableType::I32)
+    }
+
+    pub fn new_array(typ: VariableType, dimensions: Vec<usize>) -> Variable {
+        Variable {
+            variable_type: typ,
+            dimensions
+        }
+    }
+
+    pub fn f32_array(dimensions: Vec<usize>) -> Variable {
+        Self::new_array(VariableType::F32, dimensions)
+    }
+
+    pub fn i32_array(dimensions: Vec<usize>) -> Variable {
+        Self::new_array(VariableType::I32, dimensions)
+    }
+
+    pub fn get_type(&self) -> VariableType {
+        self.variable_type
+    }
+
+    pub fn is_f32(&self) -> bool {
+        self.variable_type == VariableType::F32
+    }
+
+    pub fn is_i32(&self) -> bool {
+        self.variable_type == VariableType::I32
+    }
+
+    pub fn borrow_dimensions(&self) -> &Vec<usize> {
+        &self.dimensions
+    }
+
+    pub fn get_physical_size(&self) -> usize {
+        let mut size = 1;
+        for dim in self.dimensions.iter() {
+            size *= *dim;
+        }
+        size
+    }
 }
