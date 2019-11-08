b8aed7cf544c8b5eac381ad0e17356383aa34c55
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2019-11-08 14:13:50 -0800

    Targets of assignments now use replacements properly.

--- src/vague/simplifier/helpers.rs
+++ src/vague/simplifier/helpers.rs
@@ -126,10 +126,15 @@ impl<'a> ScopeSimplifier<'a> {
         access_expression: &Expression,
     ) -> Result<(Expression, DataType), CompileProblem> {
         Result::Ok(match access_expression {
-            Expression::Variable(id, position) => (
-                Expression::Variable(self.convert(*id), position.clone()),
-                self.program[self.convert(*id)].borrow_data_type().clone(),
-            ),
+            Expression::Variable(id, position) => if let Some(new_expr) = self.replace(*id) {
+                let cloned = new_expr.clone();
+                self.simplify_assignment_access_expression(&cloned)?
+            } else {
+                (
+                    Expression::Variable(self.convert(*id), position.clone()),
+                    self.program[self.convert(*id)].borrow_data_type().clone(),
+                )
+            },
             Expression::Access {
                 base,
                 indexes,
