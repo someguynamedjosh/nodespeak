bfe95b433e73acccd7994f4e7fd2816ce1f08c3b
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2019-11-13 14:05:56 -0800

    Started creating an independent resolved phase.

---
+++ src/resolved/structure/data_type.rs
@@ -0,0 +1,127 @@
+use std::fmt::{self, Debug, Formatter};
+
+#[derive(Clone, PartialEq)]
+pub enum BaseType {
+    Bool,
+    Int,
+    Float,
+}
+
+impl BaseType {
+    pub fn equivalent(&self, other: &BaseType) -> bool {
+        self == other
+    }
+
+    pub fn to_scalar_type(self) -> DataType {
+        DataType::scalar(self)
+    }
+
+    pub fn to_array_type(self, sizes: Vec<u64>) -> DataType {
+        DataType::array(self, sizes)
+    }
+}
+
+impl Debug for BaseType {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        match self {
+            BaseType::Bool => write!(formatter, "Bool"),
+            BaseType::Int => write!(formatter, "Int"),
+            BaseType::Float => write!(formatter, "Float"),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub struct DataType {
+    base: BaseType,
+    dimensions: Vec<u64>,
+}
+
+impl DataType {
+    pub fn scalar(base: BaseType) -> DataType {
+        DataType {
+            base,
+            dimensions: Vec::new(),
+        }
+    }
+
+    pub fn new(base: BaseType) -> DataType {
+        Self::scalar(base)
+    }
+
+    pub fn array(base: BaseType, dimensions: Vec<u64>) -> DataType {
+        DataType { base, dimensions }
+    }
+
+    // E.G. if dimension is 5, [2]Int becomes [5][2]Int.
+    pub fn wrap_with_dimension(&mut self, dimension: u64) {
+        self.dimensions.insert(0, dimension);
+    }
+
+    // E.G. if dimensions is 5, 4, 3, then [2][2]Int becomes [5][4][3][2][2]Int.
+    pub fn wrap_with_dimensions(&mut self, mut dimensions: Vec<u64>) {
+        dimensions.append(&mut self.dimensions);
+        self.dimensions = dimensions;
+    }
+
+    // [2][3]Int being unwrapped once results in [3]Int
+    // [4][2][3]Int being unwrapped twice results in [3]Int
+    pub fn clone_and_unwrap(&self, num_unwraps: usize) -> DataType {
+        DataType {
+            base: self.base.clone(),
+            dimensions: Vec::from(&self.dimensions[num_unwraps..]),
+        }
+    }
+
+    pub fn borrow_dimensions(&self) -> &Vec<u64> {
+        &self.dimensions
+    }
+
+    pub fn set_dimensions(&mut self, new_dimensions: Vec<u64>) {
+        assert!(new_dimensions.len() == self.dimensions.len());
+        for (index, dimension) in new_dimensions.into_iter().enumerate() {
+            self.dimensions[index] = dimension;
+        }
+    }
+
+    pub fn borrow_base(&self) -> &BaseType {
+        &self.base
+    }
+
+    pub fn is_scalar(&self) -> bool {
+        self.dimensions.len() == 0
+    }
+
+    pub fn is_specific_scalar(&self, base_type: &BaseType) -> bool {
+        self.is_scalar() && &self.base == base_type
+    }
+
+    pub fn is_array(&self) -> bool {
+        !self.is_scalar()
+    }
+
+    pub fn equivalent(&self, other: &Self) -> bool {
+        // Check if the base types are equivalent.
+        if !self.base.equivalent(&other.base) {
+            return false;
+        }
+        if self.dimensions.len() != other.dimensions.len() {
+            return false;
+        }
+        for (dimension, other_dimension) in self.dimensions.iter().zip(other.dimensions.iter()) {
+            if dimension != other_dimension {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl Debug for DataType {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        for dimension in self.dimensions.iter() {
+            write!(formatter, "[{}]", dimension)?;
+        }
+        write!(formatter, "{:?}", self.base)
+    }
+}
---
+++ src/resolved/structure/known_data.rs
@@ -0,0 +1,249 @@
+use super::{BaseType, DataType};
+use crate::problem::FilePosition;
+use crate::util::NVec;
+use crate::resolved::structure::{ScopeId};
+
+use std::fmt::{self, Debug, Formatter};
+
+#[derive(Clone, Debug)]
+pub struct FunctionData {
+    body: ScopeId,
+    header: FilePosition,
+}
+
+impl PartialEq for FunctionData {
+    fn eq(&self, other: &Self) -> bool {
+        self.body == other.body
+    }
+}
+
+impl FunctionData {
+    pub fn new(body: ScopeId, header: FilePosition) -> FunctionData {
+        FunctionData { body, header }
+    }
+
+    pub fn set_header(&mut self, new_header: FilePosition) {
+        self.header = new_header;
+    }
+
+    pub fn get_header(&self) -> &FilePosition {
+        &self.header
+    }
+
+    pub fn get_body(&self) -> ScopeId {
+        self.body
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub enum KnownData {
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    Array(NVec<KnownData>),
+}
+
+impl KnownData {
+    pub fn new_array(dimensions: Vec<usize>, fill_with: KnownData) -> KnownData {
+        KnownData::Array(NVec::new(dimensions, fill_with))
+    }
+
+    pub fn collect(items: Vec<KnownData>) -> KnownData {
+        if items.len() == 0 {
+            return KnownData::new_array(vec![], KnownData::Bool(false));
+        }
+        // TODO: Ensure that each data type is compatible.
+        if let KnownData::Array(..) = items[0] {
+            let mut combined_data = Vec::with_capacity(items.len());
+            for item in items {
+                if let KnownData::Array(data) = item {
+                    combined_data.push(data);
+                } else {
+                    panic!("TODO nice error, not all data is array.");
+                }
+            }
+            KnownData::Array(NVec::collect(combined_data))
+        } else {
+            KnownData::Array(NVec::from_vec(items))
+        }
+    }
+
+    fn get_base_type(&self) -> BaseType {
+        match self {
+            KnownData::Bool(..) => BaseType::Bool,
+            KnownData::Int(..) => BaseType::Int,
+            KnownData::Float(..) => BaseType::Float,
+            _ => unreachable!(),
+        }
+    }
+
+    /// Returns Err if the value is KnownData::Unknown.
+    pub fn get_data_type(&self) -> Result<DataType, ()> {
+        Result::Ok(match self {
+            KnownData::Array(data) => DataType::array(
+                data.borrow_all_items()[0].get_base_type(),
+                data.borrow_dimensions().iter().map(|i| *i as u64).collect()
+            ),
+            _ => DataType::scalar(self.get_base_type()),
+        })
+    }
+
+    pub fn require_bool(&self) -> bool {
+        match self {
+            KnownData::Bool(value) => *value,
+            _ => panic!("Expected data to be a bool."),
+        }
+    }
+
+    pub fn require_int(&self) -> i64 {
+        match self {
+            KnownData::Int(value) => *value,
+            _ => panic!("Expected data to be an int."),
+        }
+    }
+
+    pub fn require_float(&self) -> f64 {
+        match self {
+            KnownData::Float(value) => *value,
+            _ => panic!("Expected data to be a float."),
+        }
+    }
+
+    pub fn require_array(&self) -> &NVec<KnownData> {
+        match self {
+            KnownData::Array(value) => value,
+            _ => panic!("Expected data to be an array."),
+        }
+    }
+
+    pub fn require_array_mut(&mut self) -> &mut NVec<KnownData> {
+        match self {
+            KnownData::Array(value) => value,
+            _ => panic!("Expected data to be an array."),
+        }
+    }
+
+    fn matches_base_type(&self, base_type: &BaseType) -> bool {
+        match self {
+            KnownData::Bool(_value) => {
+                if let BaseType::Bool = base_type {
+                    true
+                } else {
+                    false
+                }
+            }
+            KnownData::Int(_value) => {
+                if let BaseType::Int = base_type {
+                    true
+                } else {
+                    false
+                }
+            }
+            KnownData::Float(_value) => {
+                if let BaseType::Float = base_type {
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => panic!("No other base types exist."),
+        }
+    }
+
+    pub fn matches_data_type(&self, data_type: &DataType) -> bool {
+        match self {
+            KnownData::Array(contents) => {
+                // Check that the data has the same dimensions as the data type.
+                if contents.borrow_dimensions().len() != data_type.borrow_dimensions().len() {
+                    return false;
+                }
+                // TODO? check dimensions
+                // Check that all the elements have the correct type.
+                for item in contents.borrow_all_items() {
+                    if !item.matches_base_type(data_type.borrow_base()) {
+                        return false;
+                    }
+                }
+                true
+            }
+            _ => self.matches_base_type(data_type.borrow_base()),
+        }
+    }
+}
+
+impl Debug for KnownData {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        match self {
+            KnownData::Bool(value) => {
+                write!(formatter, "{}", if *value { "true" } else { "false" })
+            }
+            KnownData::Int(value) => write!(formatter, "{}", value),
+            KnownData::Float(value) => write!(formatter, "{}", value),
+            KnownData::Array(values) => {
+                // TODO: put array values on the same line if all the values in the array can fit
+                // on the same line.
+                let mut indent_level = 0;
+                for _ in values.borrow_dimensions() {
+                    write!(formatter, "{:indent$}[\n", "", indent = indent_level * 4)?;
+                    indent_level += 1;
+                }
+                let dimensions = values.borrow_dimensions().clone();
+                let mut current_index: Vec<_> = dimensions.iter().map(|_| 0).collect();
+                'main_loop: loop {
+                    write!(
+                        formatter,
+                        "{:indent$}{:?},\n",
+                        "",
+                        values.borrow_item(&current_index),
+                        indent = indent_level * 4
+                    )?;
+
+                    let last_dimension = current_index.len() - 1;
+                    current_index[last_dimension] += 1;
+                    let mut levels = 0;
+                    for dimension in (0..current_index.len()).rev() {
+                        if current_index[dimension] < dimensions[dimension] {
+                            continue;
+                        }
+                        levels += 1;
+                        if dimension == 0 {
+                            break 'main_loop;
+                        } else {
+                            current_index[dimension] = 0;
+                            current_index[dimension - 1] += 1;
+                        }
+                    }
+                    if levels > 0 {
+                        for dip in 1..levels {
+                            write!(
+                                formatter,
+                                "{:indent$}]\n",
+                                "",
+                                indent = (indent_level - dip) * 4
+                            )?;
+                        }
+                        write!(
+                            formatter,
+                            "{:indent$}],\n{:indent$}[\n",
+                            "",
+                            "",
+                            indent = (indent_level - levels) * 4
+                        )?;
+                        for dip in (1..levels).rev() {
+                            write!(
+                                formatter,
+                                "{:indent$}[\n",
+                                "",
+                                indent = (indent_level - dip) * 4
+                            )?;
+                        }
+                    }
+                }
+                for indent_level in (0..indent_level).rev() {
+                    write!(formatter, "{:indent$}]\n", "", indent = indent_level * 4)?;
+                }
+                write!(formatter, "")
+            }
+        }
+    }
+}
