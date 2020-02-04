fd88401b7f47239b82e7919f400721c5d3043561
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-02-04 12:25:01 -0800

    Got rid of x86asm library.

--- src/native/x86_64/ingest.rs
+++ src/native/x86_64/ingest.rs
@@ -1,5 +1,4 @@
 use std::io::Cursor;
-use x86asm::{Instruction, InstructionWriter, Mnemonic, Mode, Operand, Reg};
 
 use super::structure as o;
 use crate::specialized::structure as i;
@@ -12,30 +11,21 @@ pub fn ingest(program: &i::Program) -> o::Program {
 
 struct Assembler<'a> {
     source: &'a i::Program,
-    writer: InstructionWriter<Cursor<Vec<u8>>>,
+    target: Vec<u8>,
 }
 
 impl<'a> Assembler<'a> {
     fn new(source: &i::Program) -> Assembler {
-        let buffer = Cursor::new(Vec::new());
-        let writer = InstructionWriter::new(buffer, Mode::Long);
-        Assembler { source, writer }
+        Assembler { source, target: Vec::new() }
     }
 
     fn entry_point(&mut self) {
-        self.writer
-            .write(&Instruction::new2(
-                Mnemonic::MOV,
-                Operand::Direct(Reg::RAX),
-                Operand::Literal32(20),
-            ))
-            .unwrap();
+        self.target.push(0);
     }
 
     fn finalize(&mut self) -> o::Program {
-        let program = o::Program::new(0, 0);
-        let code_vec = self.writer.get_inner_writer_ref().get_ref();
-        println!("{} bytes of x64 code.", code_vec.len());
+        let program = o::Program::new(self.target.len(), 0);
+        println!("{} bytes of x64 code.", self.target.len());
         program
     }
 }
